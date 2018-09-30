eba7244f4f4225e9c5252bb59baf32074dd261cf
Carlsilber <carl.silber74@gmail.com>
2018-09-30 21:59:36 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Project Management Application
+
+
+The application is a simple Spring Boot 2 / React application
---
+++ src/main/java/com/carlsilber/pm_backend/PmBackendApplication.java
@@ -0,0 +1,12 @@
+package com.carlsilber.pm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmBackendApplication.class, args);
+	}
+}
