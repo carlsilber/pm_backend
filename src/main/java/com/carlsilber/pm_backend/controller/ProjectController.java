1002d8cb147be35fecbb56cec7882aff0f0291f9
Carlsilber <carl.silber74@gmail.com>
2018-10-05 22:43:23 +0300

    Fixed ProjectController from @Controller to @RestController

--- src/main/java/com/carlsilber/pm_backend/controller/ProjectController.java
+++ src/main/java/com/carlsilber/pm_backend/controller/ProjectController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/project")
 public class ProjectController {
 
