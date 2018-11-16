d8f151b67961f1f2c5d02c506cf03c42728ea92d
Carlsilber <carl.silber74@gmail.com>
2018-11-16 17:48:22 +0200

    Added methods to get project backlog

--- src/main/java/com/carlsilber/pm_backend/controller/BacklogController.java
+++ src/main/java/com/carlsilber/pm_backend/controller/BacklogController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/backlog")
 @CrossOrigin
 public class BacklogController {
+
   @Autowired
   private ProjectTaskService projectTaskService;
   @Autowired
@@ -26,10 +28,22 @@ public class BacklogController {
 
   @PostMapping("/{backlog_id}")
   public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask,
-      BindingResult result, @PathVariable String backlog_id){
-    ResponseEntity<?> erroMap = validationErrorService.MapValidationService(result);
-    if (erroMap != null) return erroMap;
+      BindingResult result, @PathVariable String backlog_id) {
+    //show delete
+    //custom exception
+    ResponseEntity<?> errorMap = validationErrorService.MapValidationService(result);
+    if (errorMap != null) {
+      return errorMap;
+    }
+
     ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
+
     return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
   }
+
+  @GetMapping("/{backlog_id}")
+  public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_id){
+    return projectTaskService.findBacklogById(backlog_id);
+  }
+
 }
--- src/main/java/com/carlsilber/pm_backend/repositories/ProjectTaskRepository.java
+++ src/main/java/com/carlsilber/pm_backend/repositories/ProjectTaskRepository.java
@@ -1,10 +1,13 @@
 package com.carlsilber.pm_backend.repositories;
 
 import com.carlsilber.pm_backend.entity.ProjectTask;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long>{
+
+  List<ProjectTask> findByProjectIdentifierOrderByPriority(String id);
 
 }
