bb85af55e2eb23183f963d7ca932ad7ab0843c37
Carlsilber <carl.silber74@gmail.com>
2018-11-21 12:52:47 +0200

    Finding ProjectTask by ProjectSequence

--- src/main/java/com/carlsilber/pm_backend/repositories/ProjectTaskRepository.java
+++ src/main/java/com/carlsilber/pm_backend/repositories/ProjectTaskRepository.java
@@ -10,4 +10,6 @@ public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long>
 
   List<ProjectTask> findByProjectIdentifierOrderByPriority(String id);
 
+  ProjectTask findByProjectSequence(String sequence);
+
 }
